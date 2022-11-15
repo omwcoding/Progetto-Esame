bceed8101a7afb221c7fbf50b7928c9ed52fe1b4
supersamy22 <118168524+supersamy22@users.noreply.github.com>
2022-11-15 19:27:36 +0100

    finalebellobellissimopaxxserlo

--- ProgettoEsame/funzioni.h
+++ ProgettoEsame/funzioni.h
@@ -5,3 +5,10 @@ int ** acquisizionedafile(char * p);
 
 int ** trasposta(int ** matrice);
 
+
+int ** uguaglianzamatrici(int **matrice1, int **matrice2,  int indice  );
+
+
+void stampamatrice (int ** matrice);
+
+void stampamatricefile (int ** matrice, char nomefile[]);
--- ProgettoEsame/main.c
+++ ProgettoEsame/main.c
@@ -6,12 +6,6 @@
 
 #define righe 4
 #define colonne 4
-int **acquisizionedafile(char * p);
-int ** uguaglianzamatrici(int **matrice1, int **matrice2,  int indice  );
-int ** trasposta(int ** matrice);
-void stampamatrice (int ** matrice);
-void stampamatricefile (int ** matrice, char nomefile[]);
-
 
 int main()
 {
@@ -20,60 +14,4 @@ int main()
     return 0;
 }
 
-int ** uguaglianzamatrici(int **matrice1, int **matrice2,  int indice  )
-{
-    int count = 0;
-    FILE *Uguaglianzeptr;
-    int i, j;
-    int* values = calloc(4*4, sizeof(int));
-    int** matrice3 = malloc(4* sizeof(int*));
-    for (int i=0; i<4; ++i)
-    {
-        matrice3[i] = values + i*4;
-    }
 
-    for( j = 0; j < colonne; j++ )
-    {
-        if( matrice1[indice][j] == matrice2[indice][j] )
-        {
-            count++;
-
-        }
-    }
-        if (count == colonne)
-        {
-            for( j = 0; j < colonne; j++ )
-            {
-                matrice3[indice][j] = matrice1[indice][j];
-            }
-
-        }
-        return matrice3;
-}
-void stampamatrice (int ** matrice)
-{
-        for (int i = 0; i<righe; i++)
-        {
-            printf("%d %d %d %d\n", matrice[i][0], matrice[i][1], matrice[i][2], matrice[i][3]);
-        }
-        printf("\n");
-}
-
-void stampamatricefile (int ** matrice, char nomefile[])
-{
-        FILE *fileptr;
-        if( ( fileptr = fopen(nomefile, "w") ) == NULL )
-        {
-            printf("Non e' stato possibile aprire il file!");
-            return;
-        }
-        else
-        {
-        for (int i = 0; i<righe; i++)
-        {
-            fprintf(fileptr, "%d#%d#%d#%d\n", matrice[i][0], matrice[i][1], matrice[i][2], matrice[i][3]);
-        }
-        fclose(fileptr);
-        }
-
-}
